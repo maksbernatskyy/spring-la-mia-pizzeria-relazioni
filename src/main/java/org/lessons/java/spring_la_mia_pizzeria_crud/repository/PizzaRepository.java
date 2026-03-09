218cd724dfed14bd2163d286bdd0de3c5862a88f
maksbernatskyy <maksbernatskyy@gmail.com>
2026-03-09 16:42:10 +0100

    add repository in controller

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/repository/PizzaRepository.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/repository/PizzaRepository.java
@@ -1,5 +1,8 @@
 package org.lessons.java.spring_la_mia_pizzeria_crud.repository;
 
-public class PizzaRepository {
-    
+import org.lessons.java.spring_la_mia_pizzeria_crud.model.Pizza;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PizzaRepository extends JpaRepository<Pizza, Integer> {
+
 }
