789a4a33244d1d92c90d26e9e63567bc9725f038
maksbernatskyy <maksbernatskyy@gmail.com>
2026-03-24 12:20:48 +0100

    add relation Ingrediente to Pizza

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/model/Pizza.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/model/Pizza.java
@@ -12,6 +12,9 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 
 @Entity
@@ -36,6 +39,14 @@ public class Pizza {
 
     @OneToMany(mappedBy = "pizza")
     private List<Offerta> offerta;
+
+    @ManyToMany()
+    @JoinTable(
+        name = "pizza_ingrediente",
+        joinColumns = @JoinColumn(name = "pizza_id"),
+        inverseJoinColumns = @JoinColumn(name = "ingrediente_id")
+    )
+    private List<Ingrediente> ingredienti;
 
     // Getter and Setter
 
